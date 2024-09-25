e6e6b66cf0d97e954e152b1685e9653cbe45a2f8
JoseAntonio008 <jomar.antonio62@gmail.com>
2024-09-25 12:19:48 +0800

    login and auth jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import "./App.css";
 import Login from "./pages/login";
 import Layout from "./components/ui/layout";
 import AssessmentRoll from "./pages/assessment_roll";
-import { ThemeProvider } from '@mui/material/styles';
-import theme from '../src/components/ui/theme/createtheme';
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../src/components/ui/theme/createtheme";
+import { AuthProvider } from "./auth.jsx";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Layout />}>
-          <Route path="" element={<AssessmentRoll />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Layout />}>
+              <Route path="" element={<AssessmentRoll />} />
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
